f3132c31ecba77d41f00416e60816560463881c7
ash12-coder <ashish.agrawal_cs18@gla.ac.in>
2021-06-04 15:20:55 +0800

    3 files modified

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let h = times.getHours();
 let m = times.getMinutes();
 
 document.getElementById('hour').innerHTML = h + ':' + m;
+document.getElementById('button').addEventListener('click', () => {
+  document.getElementById('radio').classList.add('active');
+});
+function cancel() {
+  document.getElementById('radio').classList.remove('active');
+}
